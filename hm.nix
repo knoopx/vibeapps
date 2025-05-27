b40da9b47a7856751878593fb2cb21dd5aa1352f
Victor Martinez <knoopx@gmail.com>
2025-05-27 21:58:07 +0200

    feat: refactor package management in flake.nix and hm.nix to streamline package calls

--- hm.nix
+++ hm.nix
@@ -6,16 +6,28 @@
 }: let
   cfg = config.programs.vibeapps;
 
-  # Get the vibeapps flake packages
-  vibeappsFlake = builtins.getFlake (toString ./.);
-  vibeappsPackages = vibeappsFlake.packages.${pkgs.system};
+  # Call packages directly using callPackage
+  vibeappsPackages = {
+    bookmarks = pkgs.callPackage ./bookmarks/bookmarks.nix {};
+    chat = pkgs.callPackage ./chat/chat.nix {};
+    dataset-viewer = pkgs.callPackage ./dataset-viewer/dataset-viewer.nix {};
+    launcher = pkgs.callPackage ./launcher/launcher.nix {};
+    md2html = pkgs.callPackage ./md2html/md2html.nix {};
+    mdx-editor = pkgs.callPackage ./mdx-editor/mdx-editor.nix {};
+    music = pkgs.callPackage ./music/music.nix {};
+    notes = pkgs.callPackage ./notes/notes.nix {};
+    raise-or-open-url = pkgs.callPackage ./raise-or-open-url/raise-or-open-url.nix {};
+    reminder = pkgs.callPackage ./reminder/reminder.nix {};
+    webkit-shell = pkgs.callPackage ./webkit-shell/webkit-shell.nix {};
+    nix-packages = pkgs.callPackage ./nix-packages/nix-packages.nix {};
+    scratchpad = pkgs.callPackage ./scratchpad/scratchpad.nix {};
+  };
 
   # Define all available programs
   programNames = [
     "bookmarks"
     "chat"
     "dataset-viewer"
-    "firefox-bookmarks"
     "launcher"
     "md2html"
     "mdx-editor"
