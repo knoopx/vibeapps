79a6a220a8c87f3caaf74fa500a5cc709cc98331
Victor Martinez <knoopx@gmail.com>
2025-05-27 20:20:35 +0200

    feat: rename scratchpad-calculator to scratchpad and update metadata

--- scratchpad/scratchpad.nix
+++ scratchpad/scratchpad.nix
@@ -2,35 +2,48 @@
   pkgs,
   lib,
   ...
-}:
-pkgs.python3Packages.buildPythonApplication {
-  name = "scratchpad-calculator";
-  src = ./scratchpad.py;
-  dontUnpack = true;
-  pyproject = false;
+}: let
+  pkg = pkgs.python3Packages.buildPythonApplication {
+    name = "scratchpad";
+    src = ./scratchpad.py;
+    dontUnpack = true;
+    pyproject = false;
 
-  nativeBuildInputs = with pkgs; [
-    wrapGAppsHook4
-    gobject-introspection
-  ];
+    nativeBuildInputs = with pkgs; [
+      wrapGAppsHook4
+      gobject-introspection
+    ];
 
-  buildInputs = with pkgs; [
-    libadwaita
-    # Add other GTK or system dependencies here if needed
-  ];
+    buildInputs = with pkgs; [
+      libadwaita
+      # Add other GTK or system dependencies here if needed
+    ];
 
-  preFixup = ''
-    gappsWrapperArgs+=(--prefix PYTHONPATH : "${pkgs.python3.withPackages (p: [
-      p.pygobject3
-    ])}/${pkgs.python3.sitePackages}")
-  '';
+    preFixup = ''
+      gappsWrapperArgs+=(--prefix PYTHONPATH : "${pkgs.python3.withPackages (p: [
+        p.pygobject3
+      ])}/${pkgs.python3.sitePackages}")
+    '';
 
-  buildPhase = ''
-    install -m 755 -D $src $out/bin/scratchpad-calculator
-  '';
+    buildPhase = ''
+      install -m 755 -D $src $out/bin/scratchpad
+    '';
 
-  meta = {
-    description = "An interactive scratchpad calculator";
-    mainProgram = "scratchpad-calculator";
+    meta = {
+      description = "An interactive scratchpad calculator";
+      mainProgram = "scratchpad";
+    };
   };
-}
+in
+  pkgs.symlinkJoin {
+    name = "scratchpad";
+    paths = [
+      pkg
+      (pkgs.makeDesktopItem {
+        name = "scratchpad";
+        desktopName = "Scratchpad";
+        exec = lib.getExe pkg;
+        icon = "accessories-calculator-symbolic";
+      })
+    ];
+  }
