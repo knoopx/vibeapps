71c50b17552015eaabaff54e8ee5d132e38bdb0e
MD. Sarwar Uddin <99182873+SrwR16@users.noreply.github.com>
2025-06-06 22:25:29 +0600

    Update md2html.nix

--- utils/md2html/md2html.nix
+++ utils/md2html/md2html.nix
@@ -8,7 +8,7 @@
     dontUnpack = true;
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
-    outputHash = "sha256-VrMoI+3lN+6KxHC5JdWpx45zCbqUowJYk+vwI4JD/S0=";
+    outputHash = "sha256-qRzV/9Kv6/gGaG0zEroh2ACxnbWaSvE4g60J2V59Jxw=";
 
     buildPhase = ''
       ${lib.getExe pkgs.bun} add rehype-autolink-headings rehype-document rehype-format rehype-highlight rehype-raw rehype-slug rehype-stringify remark-definition-list remark-frontmatter remark-gfm remark-mdx remark-oembed remark-parse remark-rehype remark-wiki-link unified
