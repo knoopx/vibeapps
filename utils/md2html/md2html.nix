0ae03f6d68034bfc8160128d27a2b97b8f400f21
MD. Sarwar Uddin <99182873+SrwR16@users.noreply.github.com>
2025-06-06 22:57:25 +0600

    Update md2html.nix

--- utils/md2html/md2html.nix
+++ utils/md2html/md2html.nix
@@ -3,18 +3,13 @@
   lib,
   ...
 }: let
-  deps = pkgs.stdenvNoCC.mkDerivation {
-    name = "md2html-deps";
-    dontUnpack = true;
-    outputHashAlgo = "sha256";
-    outputHashMode = "recursive";
-    outputHash = "sha256-qRzV/9Kv6/gGaG0zEroh2ACxnbWaSvE4g60J2V59Jxw=";
-
-    buildPhase = ''
-      ${lib.getExe pkgs.bun} add rehype-autolink-headings rehype-document rehype-format rehype-highlight rehype-raw rehype-slug rehype-stringify remark-definition-list remark-frontmatter remark-gfm remark-mdx remark-oembed remark-parse remark-rehype remark-wiki-link unified
-      mv node_modules $out
-    '';
-  };
+  deps = pkgs.runCommand "md2html-deps" {
+    nativeBuildInputs = [ pkgs.bun ];
+  } ''
+    mkdir -p $out
+    cd $out
+    ${lib.getExe pkgs.bun} add rehype-autolink-headings rehype-document rehype-format rehype-highlight rehype-raw rehype-slug rehype-stringify remark-definition-list remark-frontmatter remark-gfm remark-mdx remark-oembed remark-parse remark-rehype remark-wiki-link unified
+  '';
 in
   pkgs.stdenvNoCC.mkDerivation {
     name = "md2html";
