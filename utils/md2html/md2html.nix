f83af41e36db1f2223e13c0ebe82c18d4735a178
Victor Martinez <knoopx@gmail.com>
2025-06-11 12:37:54 +0200

    fix: prevent hash missmatch

--- utils/md2html/md2html.nix
+++ utils/md2html/md2html.nix
@@ -2,20 +2,30 @@
   pkgs,
   lib,
   ...
-}:
-pkgs.stdenvNoCC.mkDerivation {
-  name = "md2html";
-  outputHashAlgo = "sha256";
-  outputHashMode = "recursive";
-  outputHash = "sha256-Q02ANM9LZ6hIODq4/x4sAw4yQwOs5X1V3hFhnoilNEo=";
-  src = ./.;
+}: let
+  src = pkgs.stdenvNoCC.mkDerivation {
+    name = "md2html";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = "sha256-y/C7TWXhcQNrJMSDp1XsEzNtrzOMHHougL4yP4YeVZ0=";
+    src = ./.;
 
-  buildPhase = ''
-    mkdir -p $out/{bin,share/md2html}
-    substituteInPlace md2html.js --replace-fail "@@CSS@@" "md2html.css"
-    ${lib.getExe pkgs.bun} install
-    ${lib.getExe pkgs.bun} build md2html.js --outdir $out/share/md2html --target node --sourcemap --minify
-    chmod +x $out/share/md2html/md2html.js
-    ln -s $out/share/md2html/md2html.js $out/bin/md2html
-  '';
-}
+    buildPhase = ''
+      ${lib.getExe pkgs.bun} install
+      ${lib.getExe pkgs.bun} build md2html.js --outdir $out --target node --sourcemap --minify
+    '';
+  };
+in
+  pkgs.stdenvNoCC.mkDerivation {
+    name = "md2html";
+    src = src;
+
+    buildPhase = ''
+      mkdir -p $out/{bin,share/md2html}
+      cp ${./md2html.css} $out/share/md2html/md2html.css
+      substituteInPlace md2html.js --replace-fail "@@CSS@@" "$out/share/md2html/md2html.css"
+      cp md2html.js $out/share/md2html/md2html.js
+      chmod +x $out/share/md2html/md2html.js
+      ln -s $out/share/md2html/md2html.js $out/bin/md2html
+    '';
+  }
